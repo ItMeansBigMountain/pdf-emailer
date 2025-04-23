12d882c457950f4495c19666965f43f4e980bc50
Affan <affan.fareed@gmail.com>
2025-04-22 22:12:02 -0700

    main()

--- function_app/generate-newsletter/__init__.py
+++ function_app/generate-newsletter/__init__.py
@@ -9,7 +9,7 @@ app = func.FunctionApp()
 
 @app.function_name(name="GenerateNewsletter")
 @app.route(route="generate-newsletter", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
-def generate_newsletter(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         # Parse request data
         data = req.get_json()
